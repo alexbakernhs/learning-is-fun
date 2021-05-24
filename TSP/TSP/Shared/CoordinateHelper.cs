17f3d913fbc4fbf7e7a388f877970d6b1f797e80
Alex Baker <alex.baker6@nhs.net>
2021-05-24 13:43:43 +0000

    removed tracking of files and added a gitignore

---
+++ TSP/TSP/Shared/CoordinateHelper.cs
@@ -0,0 +1,12 @@
+namespace TSP
+{
+    using System;
+    using System.Collections.Generic;
+    public class CoordinateHelper
+    {
+        public IEnumerable<Coordinate> GenerateCoords(bool random = false, int size = 0)
+        {
+            return new List<Coordinate>();
+        }
+    }
+}
