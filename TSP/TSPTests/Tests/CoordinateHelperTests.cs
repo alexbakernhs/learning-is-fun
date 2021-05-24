6fd76eacbb47d14b74a6194eef34c4a744e7f14e
Alex Baker <alex.baker6@nhs.net>
2021-05-24 21:29:45 +0000

    added test to make sure that all coords returned are unique

--- TSP/TSPTests/Tests/CoordinateHelperTests.cs
+++ TSP/TSPTests/Tests/CoordinateHelperTests.cs
@@ -10,18 +10,20 @@ namespace Tests
     public class UnitTest1
     {
         private Mock<IConfigurationBuilderWrapper> _configWrapper;
-        private void Setup(bool overrideConfigWrapper = false)
+        private Mock<IRandomWrapper> _randomWrapper;
+        private void Setup(bool overrideConfigWrapper = false, bool overrideRandomWrapper = false)
         {
             InitialiseMocks();
-            MockSetups(overrideConfigWrapper);
+            MockSetups(overrideConfigWrapper, overrideRandomWrapper);
         }
 
         private void InitialiseMocks()
         {
             _configWrapper = new Mock<IConfigurationBuilderWrapper>();
+            _randomWrapper = new Mock<IRandomWrapper>();
         }
 
-        private void MockSetups(bool overrideConfigWrapper = false)
+        private void MockSetups(bool overrideConfigWrapper = false, bool overrideRandomWrapper = false)
         {
             if(!overrideConfigWrapper) _configWrapper.Setup(s => s.GetCoordinatesSection()).Returns(new List<int[]>{
                 new int[2]{1,2},
@@ -29,12 +31,21 @@ namespace Tests
                 new int[2]{5,6},
                 new int[2]{7,8}
             });
+            if(!overrideRandomWrapper) _randomWrapper.SetupSequence(s => s.Next(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(1)
+                .Returns(2)
+                .Returns(3)
+                .Returns(4)
+                .Returns(5)
+                .Returns(6)
+                .Returns(7)
+                .Returns(8);
         }
 
-        private CoordinateHelper GetCoordinateHelper(bool overrideConfigWrapper = false)
+        private CoordinateHelper GetCoordinateHelper(bool overrideConfigWrapper = false, bool overrideRandomWrapper = false)
         {
             Setup();
-            return new CoordinateHelper(_configWrapper.Object);
+            return new CoordinateHelper(_configWrapper.Object, _randomWrapper.Object);
         }
         
         [Fact]
@@ -59,6 +70,39 @@ namespace Tests
 
             //Act & Assert
             Assert.Throws<ArgumentException>(() => helper.GenerateCoords());
+        }
+
+        [Fact]
+        public void GenerateCoords_GenerateRandom_Success()
+        {
+            //Arrange
+            var helper = GetCoordinateHelper();
+
+            //Act
+            var coords = helper.GenerateCoords(true, 4, 0, 10, 0, 10);
+
+            //Assert
+            Assert.Equal(4, coords.Count());
+        }
+
+        [Fact]
+        public void GenerateCoords_GenerateRandom_NoDuplicates()
+        {
+            //Arrange
+            var helper = GetCoordinateHelper(false, true);
+            _randomWrapper.SetupSequence(s => s.Next(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(1)
+                .Returns(2)
+                .Returns(1)
+                .Returns(2)
+                .Returns(3)
+                .Returns(4);
+
+            //Act
+            var coords = helper.GenerateCoords(true, 2, 0, 4, 0, 4);
+
+            //Assert
+            Assert.False(coords.DoesHaveDuplicates());
         }
     }
 }
