f550777865a4419e306d28255347c1930722fe6e
alexbakernhs <78748282+alexbakernhs@users.noreply.github.com>
2021-05-25 00:20:56 +0100

    Added loop to generate all possible routes

--- TSP/TSP/Shared/RouteHelper.cs
+++ TSP/TSP/Shared/RouteHelper.cs
@@ -11,7 +11,36 @@ namespace TSP
                 indexes.Add(i);
             }
             
-            throw new System.NotImplementedException();
+            foreach (var permu in Permutate(seq, seq.Count))
+            {
+                foreach (var i in permu)
+                {
+                    Console.Write(i.ToString() + " ");
+                }
+                Console.WriteLine();
+                count++;
+            }
+        }
+        
+        public void RotateRight(List<int> sequence, int count)
+        {
+            int tmp = sequence[count-1];
+            sequence.RemoveAt(count - 1);
+            sequence.Insert(0, tmp);
+        }
+        
+        public IEnumerable<List<int>> Permutate(List<int> sequence, int count)
+        {
+            if (count == 1) yield return sequence;
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    foreach (var perm in Permutate(sequence, count - 1))
+                        yield return perm;
+                    RotateRight(sequence, count);
+                }
+            }
         }
     }
 }
